a04288660991f175ca944f8430229f17613ed85b
Stephen <github@c.satoristephen.com>
2016-06-30 22:01:50 -0400

    minor edit

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 /** Configuration file for ResourceSpace plugin "rs_barcode"
  *
- * These are the default values. They can be overridden by using /plugins/ocrstream/pages/setup.php
+ * These are the default values. They can be overridden by using /plugins/rs_barcode/pages/setup.php
  * which is invoked by choosing Team Centre > Manage Plugins and then clicking on Options for the
  * rs_barcode plugin once it has been activated.
  */
